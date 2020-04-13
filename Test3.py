5e8c72a7d9b65af8c188007e39def209307406d5
AIKENH <h.aiken.970@gmail.com>
2020-04-14 00:34:25 +0800

    add readme v1

--- Test3.py
+++ Test3.py
@@ -2,23 +2,9 @@
 2020-04-03 Aiken Hong 洪振鑫
 多传感信息融合作业3： 
 集中式，分布式估计融合系统
+TODO：Define：状态演化方程(线性) + num_sensor & 各自的测量方程和噪声
 TODO：Write a report  
 实验内容，实验原理，实验场景，实验结果展示和分析
-'''
-
-'''         Infomation Collect(思路整理)
-TODO：Define：状态演化方程 + num_sensor & 各自的测量方程和噪声
------------------------------------------------------------------
-TODO：分布式融合： 
-在每个sensor上使用（KF：PPT4-1：32），得到一个滤波估计，将滤波估计(Track跟踪轨迹)，
-（和方差啥？估计误差）的传送到Center
-Center做 Track 的融合: PPT4-2：107
------------------------------------------------------------------
-TODO：集中式融合：
-将每个sensor的观测，观测矩阵，噪声，传递到Center，
-然后使用基于信息滤波器的方法做最终的滤波(Track跟踪) Slide4-2：96
------------------------------------------------------------------
-TODO：输出tracking的结果和origin track以及协方差椭圆来展示效果
 '''
 
 import numpy as np 
@@ -29,19 +15,41 @@ from tqdm import tqdm
 import time 
 
 '''------------------Utility Function--------------------------'''
+'''
+TODO：输出tracking的结果和origin track以及协方差椭圆来展示效果
+'''
 def LossEllipse():
     '''误差椭圆绘制'''
     pass
 
+def PlotTrack():
+    '''绘制估计的轨迹和根据状态演化方程产生的轨迹'''
+    
+    pass
+
+
 '''-------------Distributed Fusion Function--------------------'''
+'''
+TODO：分布式融合： 
+在每个sensor上使用（KF：PPT4-1：32），得到一个滤波估计，将滤波估计(Track跟踪轨迹)，
+（和方差啥？估计误差）的传送到Center
+Center做 Track 的融合: PPT4-2：107
+'''
 def DistributedFusion():
     '''分布式融合入口函数'''
     pass
 
+
 '''---------------Central Fusion Function----------------------'''
+''' 
+TODO：集中式融合：
+将每个sensor的观测，观测矩阵，噪声，传递到Center，
+然后使用基于信息滤波器的方法做最终的滤波(Track跟踪) Slide4-2：96
+'''
 def CentralFusion():
     '''集中式融合入口函数'''
     pass
+
 
 '''-------------------Intro-----------------------'''
 if __name__ == "__main__":
---
+++ readme.md
@@ -0,0 +1,23 @@
+# Multi-Sensor Fusion
+## Intro:
+仅仅为多传感信息融合的课程作业<br>
+见仁见智，慎用代码 <br>
+<p align='right'>AIKEN-H<br></p> 
+
+---
+## Contents：
+
+1. Centralized & Distributed Fusion based on linear states transfer equation. <br>
+    线性状态转移方程下的集中式和分布式多传感信息融合
+    >include:  
+    >Kalman Fliter 卡尔曼滤波<br>
+    >information fusion 信息融合<br>
+    >
+2. 
+
+--- 
+## Something else:
+
+### <center>水平有限,请多多指教</center>
+<center>Ability is Limited；Appreciate Comments</center>
+<p align='right'>2020-04-14(origin)</p>
