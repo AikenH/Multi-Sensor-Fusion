037c70de1cf9c6d0ae8443916c05484f89d8d7a5
AIKENH <h.aiken.970@gmail.com>
2020-04-13 22:22:21 +0800

    push origin version

---
+++ Test3.py
@@ -0,0 +1,61 @@
+'''          FUNCTION DOC
+2020-04-03 Aiken Hong 洪振鑫
+多传感信息融合作业3： 
+集中式，分布式估计融合系统
+TODO：Write a report  
+实验内容，实验原理，实验场景，实验结果展示和分析
+'''
+
+'''         Infomation Collect(思路整理)
+TODO：Define：状态演化方程 + num_sensor & 各自的测量方程和噪声
+-----------------------------------------------------------------
+TODO：分布式融合： 
+在每个sensor上使用（KF：PPT4-1：32），得到一个滤波估计，将滤波估计(Track跟踪轨迹)，
+（和方差啥？估计误差）的传送到Center
+Center做 Track 的融合: PPT4-2：107
+-----------------------------------------------------------------
+TODO：集中式融合：
+将每个sensor的观测，观测矩阵，噪声，传递到Center，
+然后使用基于信息滤波器的方法做最终的滤波(Track跟踪) Slide4-2：96
+-----------------------------------------------------------------
+TODO：输出tracking的结果和origin track以及协方差椭圆来展示效果
+'''
+
+import numpy as np 
+import matplotlib.pyplot as plt 
+from scipy.stats import norm
+import math 
+from tqdm import tqdm 
+import time 
+
+'''------------------Utility Function--------------------------'''
+def LossEllipse():
+    '''误差椭圆绘制'''
+    pass
+
+'''-------------Distributed Fusion Function--------------------'''
+def DistributedFusion():
+    '''分布式融合入口函数'''
+    pass
+
+'''---------------Central Fusion Function----------------------'''
+def CentralFusion():
+    '''集中式融合入口函数'''
+    pass
+
+'''-------------------Intro-----------------------'''
+if __name__ == "__main__":
+
+    # fw = 10
+    # x = np.linspace(-100,100,1000)
+    # mean0 = 0.0
+    # var0 = 20.0
+    # plt.figure(figsize=(fw,5))
+    # plt.plot(x,norm.pdf(x, mean0, var0), label='Normal Distribution')
+    # plt.ylim(0, 0.1);
+    # plt.legend(loc='best');
+    # plt.xlabel('Position');
+    # plt.show()
+
+    print(__doc__)
+
